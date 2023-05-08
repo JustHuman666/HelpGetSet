bc99cba2c10ccabd87ac0e615f9673bed48e33b1
JustHuman666 <e.myhalchuk@gmail.com>
2023-05-09 01:37:12 +0200

    added error handling

---
+++ BLL/Validation/HelpSiteException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace BLL.Validation
+{
+    [Serializable()]
+    public class HelpSiteException : Exception
+    {
+        public HelpSiteException() : base() { }
+
+        public HelpSiteException(string message) : base(message) { }
+
+        public HelpSiteException(string message, Exception inner) : base(message, inner) { }
+
+        protected HelpSiteException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
