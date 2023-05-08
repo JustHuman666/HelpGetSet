6ecc7aef4990b5c8c5f6106c0f7a0e4f5910c888
JustHuman666 <e.myhalchuk@gmail.com>
2023-05-09 01:39:30 +0200

    added a class for authentication key creation

---
+++ BLL/JvtAuthOptions/AuthOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BLL.JvtAuthOptions
+{
+    public class AuthOptions
+    {
+        public static string? Issuer { get; set; }
+
+        public static string? Audience { get; set; }
+
+        public static string? Key { get; set; }
+
+        public AuthOptions(string issuer, string audience, string key)
+        {
+            Issuer = issuer;
+            Audience = audience;
+            Key = key;
+        }
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+        }
+    }
+}
